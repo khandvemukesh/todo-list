f4a0f57eef5d247853a7e373be6c60c61bd0924b
khandvemukesh <khandvemukesh@gmail.com>
2023-09-21 16:26:49 +0530

    TOdo login and registions api

---
+++ server/src/app.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import AuthMiddleware from "./middlewares/AuthMidleware.js";
+import apiRoute, { apiProtected } from "./routes/api.js";
+const app = express();
+const DB_CONNECT="mongodb+srv://khandvemontu4:g9H8Ib0HkVyhAzdN@todocluster.rozshki.mongodb.net/toDoList?retryWrites=true&w=majority";
+mongoose.set("strictQuery", false);
+mongoose.connect(DB_CONNECT, {useNewUrlParser:true}, (e)=>console.log(e));
+
+
+
+mongoose.connect(DB_CONNECT, { useNewUrlParser: true }, (e) => console.log(e));
+
+const PORT = 8000;
+
+app.use(express.json());
+app.use("/api/", apiRoute);
+app.use("/api/", AuthMiddleware, apiProtected);
+
+app.listen(PORT, () => console.log("server is running"));
---
+++ server/src/utils/constants.js
@@ -0,0 +1,11 @@
+export const DB_CONNECT =
+  "mongodb+srv://kartikcode123:kartik125@cluster0.p5lzc.mongodb.net/todoproject?retryWrites=true&w=majority";
+
+export const JWT_TOKEN_SECRET = "shjfsdkfjksdfks";
+
+export const StatusCode = {
+  SUCCESS: 200,
+  VALIDATION_ERROR: 201,
+  UNPROCESSABLE_ENTITY: 202,
+  AUTH_ERROR: 203,
+};
